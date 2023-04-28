d2c5506611d4a86a19679b9448cdc3c664a6513e
jared-heidt <jared14heidt@gmail.com>
2023-04-28 12:11:29 -0400

    implemented main control flow

--- main.c
+++ main.c
@@ -1,3 +1,20 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "parser.c"
+#include "executor.c"
+
 int main() {
-    return 0;
+    while (true) {
+        printf("> ");
+        char *line = read_line();
+        char **tokens = split_line(line);
+
+        if (tokens[0] != NULL) {
+            execute_command(tokens);
+        }
+
+        free(tokens);
+        free(line);
+    }
 }
